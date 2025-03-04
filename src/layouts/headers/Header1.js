24a953d16c8017ecf5c607b68f34fa9a9bd72e81
Khalid Umar <dreamiwoarld.online@gmail.com>
2025-03-04 15:33:53 +0600

    project init done

---
+++ src/layouts/headers/Header1.js
@@ -0,0 +1,163 @@
+import Link from "next/link";
+import React from "react";
+import { About, Blog, Contact, Home, Listing, Pages } from "../Menu";
+
+const Header1 = () => {
+  return (
+    <header className="header-area header-area-one d-none d-xl-block">
+      <div className="header-top">
+        <div className="container-fluid">
+          <div className="row">
+            <div className="col-md-4">
+              <div className="top-social">
+                <ul className="social-link">
+                  <li>
+                    <span>Follow us:</span>
+                  </li>
+                  <li>
+                    <a href="#">
+                      <i className="ti-facebook"></i>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#">
+                      <i className="ti-twitter-alt"></i>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#">
+                      <i className="ti-pinterest"></i>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#">
+                      <i className="ti-dribbble"></i>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#">
+                      <i className="ti-instagram"></i>
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div className="col-md-4">
+              <div className="top-content text-center">
+                <p>
+                  We Have Special Offers Every{" "}
+                  <Link href="/">Find your offer</Link>
+                </p>
+              </div>
+            </div>
+            <div className="col-md-4">
+              <div className="top-right">
+                <ul className="d-flex">
+                  <li>
+                    <Link href="/">
+                      <i className="ti-search"></i>
+                      <span>Search here</span>
+                    </Link>
+                  </li>
+                  <li>
+                    <Link href="/">
+                      <i className="ti-heart"></i>
+                      <span>Wishlist</span>
+                    </Link>
+                  </li>
+                  <li>
+                    <Link href="/">
+                      <i className="ti-shopping-cart"></i>
+                      <span>Cart</span>
+                    </Link>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="header-navigation">
+        <div className="container-fluid">
+          <div className="primary-menu">
+            <div className="row">
+              <div className="col-lg-2 col-5">
+                <div className="site-branding">
+                  <Link className="brand-logo" href="/">
+                    <img src="assets/images/logo/logo-1.png" alt="Brand Logo" />
+                  </Link>
+                </div>
+              </div>
+              <div className="col-lg-6 col-2">
+                <div className="nav-menu">
+                  <div className="navbar-close">
+                    <i className="ti-close"></i>
+                  </div>
+                  <nav className="main-menu">
+                    <ul>
+                      <li className="menu-item has-children">
+                        <Link href="/">Home</Link>
+                        <ul className="sub-menu">
+                          <Home />
+                        </ul>
+                      </li>
+                      <About />
+                      <li className="menu-item has-children">
+                        <a href="#">Listings</a>
+                        <ul className="sub-menu">
+                          <Listing />
+                        </ul>
+                      </li>
+                      <li className="menu-item has-children">
+                        <a href="#">Pages</a>
+                        <ul className="sub-menu">
+                          <Pages />
+                        </ul>
+                      </li>
+                      <li className="menu-item has-children">
+                        <a href="#">Article</a>
+                        <ul className="sub-menu">
+                          <Blog />
+                        </ul>
+                      </li>
+                      <Contact />
+                      <li className="nav-btn">
+                        <Link className="main-btn icon-btn" href="/add-listing">
+                          Add Listing
+                        </Link>
+                      </li>
+                    </ul>
+                  </nav>
+                </div>
+              </div>
+              <div className="col-lg-4 col-5">
+                <div className="header-right-nav">
+                  <ul className="d-flex align-items-center">
+                    <li className="user-btn">
+                      <Link className="icon" href="/">
+                        <i className="flaticon-avatar"></i>
+                      </Link>
+                    </li>
+                    <li className="hero-nav-btn">
+                      <Link className="main-btn icon-btn" href="/add-listing">
+                        Add Listing
+                      </Link>
+                    </li>
+                    <li className="nav-toggle-btn">
+                      <div className="navbar-toggler">
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                      </div>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
+export default Header1;
